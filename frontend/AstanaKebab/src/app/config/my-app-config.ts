15639abde4300c6ce145c188107627e6898041c5
Aybolali <110097999+aybolali@users.noreply.github.com>
2025-04-07 23:36:06 +0200

    secret

--- frontend/AstanaKebab/src/app/config/my-app-config.ts
+++ frontend/AstanaKebab/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
     //OpenIDConnect
     oidc : {
-        clientId: '0oalph6c8dOEJDLvC5d7',
-        issuer: 'https://dev-21036721.okta.com/oauth2/default', //MUST USE HTTPS
+        clientId: '*',
+        issuer: '*', //MUST USE HTTPS
         redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'] //info about user  -- openid not openId
     }
