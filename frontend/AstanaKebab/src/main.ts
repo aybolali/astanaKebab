2cac74577fb7e22359f7fc923a8c8244d9fe6947
aybolali <asartbai7>
2025-04-27 18:07:04 +0200

    main updates

--- frontend/AstanaKebab/src/main.ts
+++ frontend/AstanaKebab/src/main.ts
@@ -1,8 +1,11 @@
 /// <reference types="@angular/localize" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { environment } from './environments/environment';
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
+
+console.log('API URL:', environment.astanaKebabApiUrl);
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
